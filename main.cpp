78a89f6c129e13641815c65c91956856cb98370e
Artemiy987 <98634526+Artemiy987@users.noreply.github.com>
2022-06-06 13:27:39 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!"<<" fefds" << endl;
+    int x;
+    int y;
+    cin >> x >> y;
+    cout<<x <<' '<<y;
+    return 0;
+}
